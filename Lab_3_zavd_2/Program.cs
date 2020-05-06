f2c04303b91bb116869cbfd5f16664901830d3d0
VladGrz <60891301+VladGrz@users.noreply.github.com>
2020-05-06 16:49:43 +0300

    zavdannia_2

---
+++ Lab_3_zavd_2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+namespace Lab_3_zavd_2
+{
+    class Program
+    {
+        static void WriteInFile(string a) {
+            string[] numbers = a.Split(" ");
+
+            var zapis = new StreamWriter("D:/ООП/Lab_3/Lab_3_zavd_2/Lab_3_zavd_2/Numbers.txt", true);
+            zapis.Write(Environment.NewLine);
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (int.Parse(numbers[i]) % 2 == 0 && (i + 1) % 2 != 0)
+                {
+                    zapis.Write(numbers[i] + " ");
+                }
+            }
+            zapis.Close();
+            Console.WriteLine("Вміст файлу після виконання програми:\n" + File.ReadAllText("D:/ООП/Lab_3/Lab_3_zavd_2/Lab_3_zavd_2/Numbers.txt"));
+            
+        }
+        static void Main(string[] args)
+        {
+            string text = File.ReadAllText("D:/ООП/Lab_3/Lab_3_zavd_2/Lab_3_zavd_2/Numbers.txt");
+            Console.WriteLine("Вміст файлу:\n" + text);
+            WriteInFile(text);
+        }
+    }
+}
--- Lab_3_zavd_2/obj/Debug/netcoreapp3.1/Lab_3_zavd_2.AssemblyInfo.cs
+++ Lab_3_zavd_2/obj/Debug/netcoreapp3.1/Lab_3_zavd_2.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Lab_3")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Lab_3_zavd_2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("Lab_3")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Lab_3")]
+[assembly: System.Reflection.AssemblyProductAttribute("Lab_3_zavd_2")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Lab_3_zavd_2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Создано классом WriteCodeFragment MSBuild.
